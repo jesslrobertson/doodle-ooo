283e980c94aee6ebca6b0951c46c22210c389b33
Christopher Miller <chris.miller0686@gmail.com>
2022-08-23 14:59:31 -0600

    put request/error handling

--- routes/galleryRouter.js
+++ routes/galleryRouter.js
@@ -1,8 +1,27 @@
 const express = require("express")
 const galleryRouter = express.Router()
+const Artwork = require("../models/Artwork")
 
 galleryRouter.get("/", (req, res, next) => {
-    console.log(res)
+    Artwork.find((err, items) => {
+        if (err) {
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(items)
+    })
+})
+
+
+galleryRouter.post('/', (req, res, next) => {
+    const newArt = new Artwork(req.body)
+    newArt.save((err, savedArtwork) => {
+        if (err) {
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(savedArtwork)
+    })
 })
 
 
