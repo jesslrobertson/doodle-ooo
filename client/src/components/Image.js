3e792580d8090f67b07e12c7741ed1abe710c1b2
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-08-24 14:06:47 -0600

    formatted with prettier, cleaned up a console.log

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -1,30 +1,29 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
-export default function Image(props){
-  const {Url, _id, setGalleryImages} = props
+export default function Image(props) {
+  const { Url, _id, setGalleryImages } = props;
 
-  function removeImage(imgID){
-    console.log(console.log(`delete request id is ${imgID}`))
-    axios.delete(`/gallery/${imgID}`)
-    .then(res => {
-      setGalleryImages(prevImages => prevImages.filter(image => image._id !== imgID))
-    })
-    .catch((err) => console.log(err))
+  function removeImage(imgID) {
+    axios
+      .delete(`/gallery/${imgID}`)
+      .then((res) => {
+        setGalleryImages((prevImages) =>
+          prevImages.filter((image) => image._id !== imgID)
+        );
+      })
+      .catch((err) => console.log(err));
   }
-  return(
+  return (
     <div className="img-container">
-      <img 
-        src={Url}
-        _id={_id}
-        className="gallery-img"
-      />
+      <img src={Url} _id={_id} className="gallery-img" />
       <button
         onClick={() => {
-          removeImage(_id)}}
+          removeImage(_id);
+        }}
       >
         delete
       </button>
     </div>
-  )
+  );
 }
--- client/src/pages/Studio.js
+++ client/src/pages/Studio.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import Canvas from "../components/Canvas"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import Canvas from "../components/Canvas";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function Studio(){
+export default function Studio() {
   const notify = (message) => toast(message);
 
   return (
     <div className="page studio">
       <h1>This is the Studio Page</h1>
       <ToastContainer />
-      <Canvas 
-        notify={notify}
-      />
+      <Canvas notify={notify} />
     </div>
-  )
+  );
 }
