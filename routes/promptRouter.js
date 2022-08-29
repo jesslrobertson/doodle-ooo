063dd9268d01f17ccd4f5fcd4987efeab1c735c9
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-08-29 13:53:34 -0600

    Added back and front end requets for getting a single random drawing prompt

---
+++ routes/promptRouter.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const promptRouter = express.Router()
+const Prompt = require('../models/Prompt')
+
+//add prompts to database
+// Prompt.insertMany(
+//   [
+//     //paste array of json prompt objects here
+// ]
+// ).then(function(){
+//   console.log("Data inserted")  // Success
+// }).catch(function(error){
+//   console.log(error)      // Failure
+// });
+
+promptRouter.get("/", (req, res, next) => {
+    Prompt.find((err, items) => {
+        if (err) {
+            res.status(500);
+            return next(err);
+          }
+        const count = items.length
+        const randomItem = Math.floor(Math.random() * count)
+        return res.status(200).send(items[randomItem]);
+        });
+      });
+      
+      
+
+
+module.exports = promptRouter
