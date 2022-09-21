6d945d2fb2dda19a5f2e5750a58693316514691b
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-09-21 17:44:18 -0600

    CORS configuration and netlify.toml changes

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const dotenv = require("dotenv");
 const cors = require("cors")
 
 dotenv.config();
-app.use(cors())
+app.use(cors({
+  origin: true
+}))
 app.use(express.json());
 app.use(morgan("dev"));
 // app.use(function(req, res, next) {
